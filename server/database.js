67e6df73fa83c0af71d88395bd22816954e7cf5c
isurika <isurikalakshani2001@gmail.com>
2023-12-12 16:43:43 +0530

    database connection

--- server/database.js
+++ server/database.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 async function connectToDatabase() {
   const connectionString =
-    "mongodb://<username>:<password>@<remote-host>:<port>/<database-name>";
+    "mongodb+srv://techceee:BWB8zIPsDxHi6v6Q@crm.oe1aqip.mongodb.net/?retryWrites=true&w=majority";
 
   try {
-    // await mongoose.connect(connectionString, {
-    await mongoose.connect("mongodb://localhost:27017/school", {
+    await mongoose.connect(connectionString, {
+    // await mongoose.connect("mongodb://localhost:27017/school", {
       // Local connection - testing purposes
       useNewUrlParser: true,
       useUnifiedTopology: true,
