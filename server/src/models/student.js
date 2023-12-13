34b87fcb46fc51c6458392b857bf5b313fb50916
hiranyasemindi <hiranyagunawardhane@gmail.com>
2023-12-13 15:56:41 +0530

    Update student.js

--- server/src/models/student.js
+++ server/src/models/student.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const studentSchema = new mongoose.Schema({
   name: String,
   age: Number,
+  contact_no: String,
+  email: String,
+  address: String
   // Add other fields as needed
 });
 
