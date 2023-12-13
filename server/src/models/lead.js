eb26329dd7211599a407b053f528c65936a8131c
hiranyasemindi <hiranyagunawardhane@gmail.com>
2023-12-13 15:57:03 +0530

    Create lead.js

---
+++ server/src/models/lead.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const leadSchema = new mongoose.Schema({
+    date: Date,
+    sheduled_at: Date,
+    sheduled_to: Date,
+    course_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Course'},
+    branch_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Branch'},
+    status_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Status'}
+})
+
+const lead = mongoose.model('Lead', leadSchema)
+
+module.exports = lead
