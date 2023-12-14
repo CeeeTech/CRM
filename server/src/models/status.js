d389313a4897c2689822f7f1e366af978c65f322
hiranyasemindi <hiranyagunawardhane@gmail.com>
2023-12-14 23:57:55 +0530

    Update status.js

--- server/src/models/status.js
+++ server/src/models/status.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const statusSchema = mongoose.Schema({
+const statusSchema = new mongoose.Schema({
     name: String
 })
 
 const status = mongoose.model('Status', statusSchema)
 
-module.exports(status)
+module.exports = status
