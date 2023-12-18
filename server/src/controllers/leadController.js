58fc967de370fc0655d2e0941f3fc04e6a52f134
hiranyasemindi <hiranyagunawardhane@gmail.com>
2023-12-19 02:40:11 +0530

    update leadController.js

--- server/src/controllers/leadController.js
+++ server/src/controllers/leadController.js
@@ -3,6 +3,7 @@ const Course = require("../models/course");
 const Branch = require("../models/branch");
 const Status = require("../models/status");
 const Student = require("../models/student");
+const { default: mongoose } = require("mongoose");
 
 //get all leads
 async function getLeads(req, res) {
@@ -15,40 +16,54 @@ async function getLeads(req, res) {
     }
 }
 
+//get one lead
+async function getLead(req, res) {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(404).json({ error: "No such lead" })
+    }
+
+    const lead = await Lead.findById({ _id: id })
+
+    if (!lead) {
+        res.status(400).json({ error: "No such lead" })
+    }
+
+    res.status(200).json(lead)
+
+}
+
 //add new lead
 async function addLead(req, res) {
     const { date, sheduled_at, sheduled_to, course_name, branch_name, status, student_name } = req.body
 
     //check if course_name exist in course table
-    const course_doucument = await Course.findOne({name: course_name})
-
-    if(!course_doucument){
-        res.status(400).json({error: `course not found: ${course_name}`})
+    const course_doucument = await Course.findOne({ name: course_name })
+    if (!course_doucument) {
+        res.status(400).json({ error: `course not found: ${course_name}` })
     }
 
     //check if branch_name exist in branch table
-    const branch_doucument = await Branch.findOne({name: branch_name})
-
-    if(!branch_doucument){
-        res.status(400).json({error: `branch not found: ${branch_name}`})
+    const branch_doucument = await Branch.findOne({ name: branch_name })
+    if (!branch_doucument) {
+        res.status(400).json({ error: `branch not found: ${branch_name}` })
     }
 
     //check if status exist in status table
-    const status_document = await Status.findOne({name: status})
-
-    if(!status_document){
-        res.status(400).json({error: `status not found: ${status}`})
+    const status_document = await Status.findOne({ name: status })
+    if (!status_document) {
+        res.status(400).json({ error: `status not found: ${status}` })
     }
 
     //check if student exist in student table
-    const student_document = await Student.findOne({name: student_name})
-
-    if(!student_document){
-        res.status(400).json({error: `student not found: ${student_name}`})
+    const student_document = await Student.findOne({ name: student_name })
+    if (!student_document) {
+        res.status(400).json({ error: `student not found: ${student_name}` })
     }
 
     try {
-        const newLead = await Lead.create({date, sheduled_at, sheduled_to, course_id: course_doucument._id, branch_id:branch_doucument._id, status_id:status_document._id, student_id:student_document._id})
+        const newLead = await Lead.create({ date, sheduled_at, sheduled_to, course_id: course_doucument._id, branch_id: branch_doucument._id, status_id: status_document._id, student_id: student_document._id })
         res.status(200).json(newLead)
     } catch (error) {
         console.log('Error adding leads:', error)
@@ -56,7 +71,30 @@ async function addLead(req, res) {
     }
 }
 
+//update lead
+async function updateLead(req, res) {
+
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: "No such lead"})
+    }
+
+    const lead = await Lead.findByIdAndUpdate({ _id: id }, {
+        ...req.body
+    })
+
+    if(!lead){
+        res.status(400).json({error: "no such lead"})
+    }
+
+    res.status(200).json(lead)
+
+}
+
 module.exports = {
     getLeads,
     addLead,
+    getLead,
+    updateLead
 }
