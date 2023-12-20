8f9d30e6e3e85da09d26486e6927ed602c04705d
hiranyasemindi <hiranyagunawardhane@gmail.com>
2023-12-21 02:52:33 +0530

    added get and add branch routes

--- server/src/routes/branchRoutes.js
+++ server/src/routes/branchRoutes.js
@@ -3,6 +3,7 @@ const branchController = require('../controllers/branchController')
 
 const router = express.Router()
 
-router.get('/', branchController.getBranches)
+router.get('/branches', branchController.getBranches)
+router.post('/branches', branchController.addBranch)
 
 module.exports = router
