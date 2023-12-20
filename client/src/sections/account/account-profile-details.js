0384f015414b5526309ff21bb6def876e09f1ef3
hiranyasemindi <hiranyagunawardhane@gmail.com>
2023-12-21 02:54:47 +0530

    add lead frontend

--- client/src/sections/account/account-profile-details.js
+++ client/src/sections/account/account-profile-details.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import {
   Box,
   Button,
@@ -11,34 +11,77 @@ import {
   Unstable_Grid2 as Grid
 } from '@mui/material';
 
-const states = [
-  {
-    value: 'alabama',
-    label: 'Alabama'
-  },
-  {
-    value: 'new-york',
-    label: 'New York'
-  },
-  {
-    value: 'san-francisco',
-    label: 'San Francisco'
-  },
-  {
-    value: 'los-angeles',
-    label: 'Los Angeles'
-  }
-];
-
 export const AccountProfileDetails = () => {
+
+  const [branches, setBranches] = useState(null);
+  const [statuses, setStatuses] = useState(null);
+  const [courses, setCourses] = useState(null);
+
+  useEffect(() => {
+    const fetchBranches = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/branches');
+        if (response.ok) {
+          const json = await response.json();
+          setBranches(json);
+          console.log(json); // Log the branches to verify the data
+        } else {
+          console.error('Error fetching branches:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching branches:', error.message);
+      }
+    };
+    const fetchStatuses = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/status');
+        if (response.ok) {
+          const json = await response.json();
+          setStatuses(json);
+          console.log(json); // Log the status to verify the data
+        } else {
+          console.error('Error fetching  status:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching status:', error.message);
+      }
+    };
+    const fetchCourses = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/courses');
+        if (response.ok) {
+          const json = await response.json();
+          setCourses(json);
+          console.log(json); // Log the course to verify the data
+        } else {
+          console.error('Error fetching courses:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching courses:', error.message);
+      }
+    };
+    fetchBranches()
+    fetchStatuses()
+    fetchCourses()
+  }, [])
+
+  const date = new Date();
+  const formattedDate = date.toISOString().split('T')[0];
+
   const [values, setValues] = useState({
-    firstName: 'Anika',
-    lastName: 'Visser',
-    email: 'demo@devias.io',
+    name: '',
+    age: '',
+    email: '',
     phone: '',
-    state: 'los-angeles',
-    country: 'USA'
+    address: '',
+    date: formattedDate,
+    scheduled_to: formattedDate,
+    course: 'Computer Science',
+    branch: 'Colombo',
+    status: 'Registered',
+    comment: ''
   });
+
 
   const handleChange = useCallback(
     (event) => {
@@ -50,11 +93,79 @@ export const AccountProfileDetails = () => {
     []
   );
 
+  useEffect(() => {
+    console.log('Updated values', values);
+  }, [values]);
+
   const handleSubmit = useCallback(
-    (event) => {
+    async (event) => {
       event.preventDefault();
+
+      try {
+        //insert student data
+        const studentResponse = await fetch('http://localhost:8080/api/students', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            name: values.name,
+            age: values.age,
+            contact_no: values.phone,
+            email: values.email,
+            address: values.address
+          })
+        });
+        if (!studentResponse.ok) {
+          console.error("Error inserting data to the student table", studentResponse.statusText)
+          return
+        }
+        const studentData = await studentResponse.json();
+        const { _id: student_id } = studentData;
+        console.log("Student ID:", student_id);
+        //insert lead data
+        const leadResponse = await fetch('http://localhost:8080/api/leads', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            "date": values.date,
+            "sheduled_to": values.scheduled_to,
+            "course_name": values.course,
+            "branch_name": values.branch,
+            "student_id": student_id
+          })
+        })
+        if (!leadResponse.ok) {
+          console.error("Error inserting data to the lead table", leadResponse.statusText);
+          return
+        }
+
+        const leadData = await leadResponse.json();
+        const { _id: lead_id } = leadData;
+        console.log("Lead ID:", lead_id);
+
+        //insert follow-up data
+        const followUpResponse = await fetch('http://localhost:8080/api/followUps', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(
+            {
+              "lead_id": lead_id,
+              "user": 'Hana Amaily',
+              "status": values.status,
+              "comment": values.comment
+            }
+          )
+        });
+        if (!followUpResponse.ok) {
+          console.error("Error inserting followup data", followUpResponse.statusText);
+          return
+        }
+        console.log('Data inserted successfully!');
+      } catch (error) {
+        console.error('Error during data insertion:', error.message);
+      }
+
     },
-    []
+    [values]
   );
 
   return (
@@ -65,7 +176,7 @@ export const AccountProfileDetails = () => {
     >
       <Card>
         <CardHeader
-          subheader="The information can be edited"
+          subheader="The  can be edited"
           title="Profile"
         />
         <CardContent sx={{ pt: 0 }}>
@@ -80,12 +191,12 @@ export const AccountProfileDetails = () => {
               >
                 <TextField
                   fullWidth
-                  helperText="Please specify the first name"
-                  label="First name"
-                  name="firstName"
+                  helperText="Please specify the student name"
+                  label="Name"
+                  name="name"
                   onChange={handleChange}
                   required
-                  value={values.firstName}
+                  value={values.name}
                 />
               </Grid>
               <Grid
@@ -94,11 +205,12 @@ export const AccountProfileDetails = () => {
               >
                 <TextField
                   fullWidth
-                  label="Last name"
-                  name="lastName"
+                  label="Age"
+                  name="age"
                   onChange={handleChange}
                   required
-                  value={values.lastName}
+                  type='number'
+                  value={values.age}
                 />
               </Grid>
               <Grid
@@ -114,6 +226,7 @@ export const AccountProfileDetails = () => {
                   value={values.email}
                 />
               </Grid>
+
               <Grid
                 xs={12}
                 md={6}
@@ -125,19 +238,22 @@ export const AccountProfileDetails = () => {
                   onChange={handleChange}
                   type="number"
                   value={values.phone}
-                />
-              </Grid>
-              <Grid
-                xs={12}
-                md={6}
-              >
-                <TextField
-                  fullWidth
-                  label="Country"
-                  name="country"
-                  onChange={handleChange}
                   required
-                  value={values.country}
+                />
+              </Grid>
+
+              <Grid
+                xs={12}
+                md={12}
+              >
+                <TextField
+                  fullWidth
+                  label="Address"
+                  name="address"
+                  onChange={handleChange}
+                  type="text"
+                  value={values.address}
+                  required
                 />
               </Grid>
               <Grid
@@ -146,30 +262,135 @@ export const AccountProfileDetails = () => {
               >
                 <TextField
                   fullWidth
-                  label="Select State"
-                  name="state"
+                  label="Date"
+                  name="date"
+                  onChange={handleChange}
+                  type="text"
+                  value={values.date}
+                  required
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Scheduled to"
+                  name="scheduled_to"
+                  onChange={handleChange}
+                  type="date"
+                  value={values.scheduled_to}
+                  required
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Select Course"
+                  name="course"
                   onChange={handleChange}
                   required
                   select
                   SelectProps={{ native: true }}
-                  value={values.state}
+                  value={values.course}
                 >
-                  {states.map((option) => (
-                    <option
-                      key={option.value}
-                      value={option.value}
-                    >
-                      {option.label}
+                  {courses && courses.length > 0 ? (
+                    courses.map((option) => (
+                      <option key={option._id} value={option.name}>
+                        {option.name}
+                      </option>
+                    ))
+                  ) : (
+                    <option value="" disabled>
+                      No Statuses available
                     </option>
-                  ))}
+                  )}
                 </TextField>
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Select Branch"
+                  name="branch"
+                  onChange={handleChange}
+                  required
+                  select
+                  SelectProps={{ native: true }}
+                  value={values.branch}
+                >
+                  {/* Ensure branches is an array and has elements */}
+                  {branches && branches.length > 0 ? (
+                    branches.map((option) => (
+                      // Ensure each option has a unique key
+                      <option key={option._id} value={option.name}>
+                        {option.name}
+                      </option>
+                    ))
+                  ) : (
+                    // Provide a default option if branches is empty or not available
+                    <option value="" disabled>
+                      No branches available
+                    </option>
+                  )}
+                </TextField>
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Select Status"
+                  name="status"
+                  onChange={handleChange}
+                  required
+                  select
+                  SelectProps={{ native: true }}
+                  value={values.status}
+                >
+                  {/* Ensure statuses is an array and has elements */}
+                  {statuses && statuses.length > 0 ? (
+                    statuses.map((option) => (
+                      // Ensure each option has a unique key
+                      <option key={option._id} value={option.name}>
+                        {option.name}
+                      </option>
+                    ))
+                  ) : (
+                    // Provide a default option if statuses is empty or not available
+                    <option value="" disabled>
+                      No Statuses available
+                    </option>
+                  )}
+                </TextField>
+              </Grid>
+              <Grid
+                xs={12}
+                md={12}
+              >
+                <TextField
+                  fullWidth
+                  label="Comment"
+                  name="comment"
+                  onChange={handleChange}
+                  type="text"
+                  value={values.comment}
+                  required
+                />
               </Grid>
             </Grid>
           </Box>
         </CardContent>
         <Divider />
         <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <Button variant="contained">
+          <Button variant="contained" type='submit'>
             Save details
           </Button>
         </CardActions>
