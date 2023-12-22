d8378d3a2cd04c263a6f27bc439b17aab59f4066
hiranyasemindi <hiranyagunawardhane@gmail.com>
2023-12-23 03:15:08 +0530

    update branchRoutes.js

--- server/src/routes/branchRoutes.js
+++ server/src/routes/branchRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router()
 
 router.get('/branches', branchController.getBranches)
 router.post('/branches', branchController.addBranch)
+router.get('/branches/:id', branchController.getBranch)
+
 
 module.exports = router
