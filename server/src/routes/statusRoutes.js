ee4ffad2f01becf8db6baca2f25f96f47f0a8501
hiranyasemindi <hiranyagunawardhane@gmail.com>
2023-12-23 03:14:27 +0530

    update statusRoutes.js

--- server/src/routes/statusRoutes.js
+++ server/src/routes/statusRoutes.js
@@ -3,6 +3,8 @@ const statusController = require('../controllers/statusController')
 
 const router = express.Router()
 
-router.get('/status', statusController.getStatus)
+router.get('/status', statusController.getAllStatus)
+router.get('/status/:id', statusController.getStatus)
+
 
 module.exports = router
