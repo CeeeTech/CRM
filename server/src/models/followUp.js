689d45c5a73bda500822d37fe93a18a5fd759456
hiranyasemindi <hiranyagunawardhane@gmail.com>
2023-12-29 12:49:17 +0530

    update follow up table

--- server/src/models/followUp.js
+++ server/src/models/followUp.js
@@ -4,7 +4,8 @@ const followUpSchema = new mongoose.Schema({
     lead_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Lead'},
     status_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Status'},
     user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    comment: String
+    comment: String,
+    date: Date
 })
 
 const follow_up = mongoose.model('Follow_Up', followUpSchema)
