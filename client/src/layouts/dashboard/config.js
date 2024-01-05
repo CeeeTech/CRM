577dc8031443434e82641bc24eb1ef682bab0512
isurika <isurikalakshani2001@gmail.com>
2024-01-05 13:10:02 +0530

    added new user menu link

--- client/src/layouts/dashboard/config.js
+++ client/src/layouts/dashboard/config.js
@@ -1,102 +1,111 @@
-import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
-import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
-import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
-import UserIcon from '@heroicons/react/24/solid/UserIcon';
-import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
-import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
-import { SvgIcon } from '@mui/material';
+import ChartBarIcon from "@heroicons/react/24/solid/ChartBarIcon";
+import CogIcon from "@heroicons/react/24/solid/CogIcon";
+import LockClosedIcon from "@heroicons/react/24/solid/LockClosedIcon";
+import ShoppingBagIcon from "@heroicons/react/24/solid/ShoppingBagIcon";
+import UserIcon from "@heroicons/react/24/solid/UserIcon";
+import UserPlusIcon from "@heroicons/react/24/solid/UserPlusIcon";
+import UsersIcon from "@heroicons/react/24/solid/UsersIcon";
+import XCircleIcon from "@heroicons/react/24/solid/XCircleIcon";
+import { SvgIcon } from "@mui/material";
 
 export const items = [
   {
-    title: 'Dashbord',
-    path: '/',
+    title: "Dashbord",
+    path: "/",
     icon: (
       <SvgIcon fontSize="small">
         <ChartBarIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Students',
-    path: '/students',
+    title: "Users",
+    path: "/users",
     icon: (
       <SvgIcon fontSize="small">
         <UsersIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Meetings',
-    path: '/meeting',
+    title: "Students",
+    path: "/students",
     icon: (
       <SvgIcon fontSize="small">
         <UsersIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Reports',
-    path: '/reports',
+    title: "Meetings",
+    path: "/meeting",
     icon: (
       <SvgIcon fontSize="small">
         <UsersIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Branches',
-    path: '/branches',
+    title: "Reports",
+    path: "/reports",
+    icon: (
+      <SvgIcon fontSize="small">
+        <UsersIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    title: "Branches",
+    path: "/branches",
     icon: (
       <SvgIcon fontSize="small">
         <ShoppingBagIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'leads',
-    path: '/leads',
+    title: "leads",
+    path: "/leads",
     icon: (
       <SvgIcon fontSize="small">
         <ShoppingBagIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Courses',
-    path: '/courses',
+    title: "Courses",
+    path: "/courses",
     icon: (
       <SvgIcon fontSize="small">
         <ShoppingBagIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Account',
-    path: '/account',
+    title: "Account",
+    path: "/account",
     icon: (
       <SvgIcon fontSize="small">
         <UserIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Settings',
-    path: '/settings',
+    title: "Settings",
+    path: "/settings",
     icon: (
       <SvgIcon fontSize="small">
         <CogIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Login',
-    path: '/auth/login',
+    title: "Login",
+    path: "/auth/login",
     icon: (
       <SvgIcon fontSize="small">
         <LockClosedIcon />
       </SvgIcon>
-    )
-  }
+    ),
+  },
 ];
